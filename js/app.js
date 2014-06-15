b143f16ec97afc052d2f7f98ba9a6139be5a25b2
Julien <Julien>
2014-06-15 16:58:52 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+(function() {
+	var app = angular.module('app', []);
+
+	app.controller('MainController', function() {
+		this.lycees = ["", "Chamalières"];
+		this.filieres = ["", "Esthétique"];
+		
+
+	});
+
+})();
